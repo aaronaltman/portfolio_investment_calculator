fc2ef807ef4c9ea597b48141a00c6dec7c62c7d2
aka <aaronkaltman@gmail.com>
2023-12-28 14:12:00 -0500

    looks like its working

--- src/components/not-layout/output-table.tsx
+++ src/components/not-layout/output-table.tsx
@@ -1,13 +1,5 @@
 import React from "react";
-import DataTable from "./table";
-import Table2 from "./table2";
 
 export default function OutputTable() {
-  return (
-    <div>
-      <section className="w-full">
-        <Table2 />
-      </section>
-    </div>
-  );
+  return <div>output-table</div>;
 }
