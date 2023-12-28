1aba9eb45312884fbd6a57278139ea010df1c111
aka <aaronkaltman@gmail.com>
2023-12-28 13:27:58 -0500

    setting state

--- src/components/not-layout/input-box.tsx
+++ src/components/not-layout/input-box.tsx
@@ -1,13 +1,37 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import { Button } from "../ui/button";
 
+type HandleInitialProps = React.ChangeEvent<HTMLInputElement>;
+
 export default function InputBox() {
-  const [initialInvestment, setInitialInvestment] = React.useState(0);
-  const [monthlyInvestment, setMonthlyInvestment] = React.useState(0);
-  const [investmentPeriod, setInvestmentPeriod] = React.useState(0);
-  const [estimatedRateOfReturn, setEstimatedRateOfReturn] = React.useState(0);
+  const [initialInvestment, setInitialInvestment] = useState("");
+  const [monthlyInvestment, setMonthlyInvestment] = useState("");
+  const [investmentPeriod, setInvestmentPeriod] = useState(0);
+  const [estimatedRateOfReturn, setEstimatedRateOfReturn] = useState(0);
+
+  const handleInitialInvestmentChange = (event: HandleInitialProps) => {
+    // Convert the input value to a number before setting the state
+    let value = Number(event.target.value);
+    if (value < 0) {
+      alert("Initial investment cannot be negative");
+      return;
+    }
+    setInitialInvestment(value.toString());
+    console.log(value);
+  };
+
+  const handleMonthlyInvestmentChange = (event: HandleInitialProps) => {
+    // Convert the input value to a number before setting the state
+    let value = Number(event.target.value);
+    if (value < 0) {
+      alert("Initial investment cannot be negative");
+      return;
+    }
+    setMonthlyInvestment(value.toString());
+    console.log(value);
+  };
 
   return (
     <section className="bg-slate-900 max-w-2xl mx-auto py-8 my-6 rounded-md shadow-md">
@@ -21,7 +45,8 @@ export default function InputBox() {
             type="number"
             placeholder="Enter your initial investment amount"
             className="mt-2 p-3 rounded border-none bg-slate-700 text-white placeholder-gray-300"
-            onChange={(e) => setInitialInvestment(e.target.value)}
+            onChange={handleInitialInvestmentChange}
+            value={initialInvestment}
           />
         </label>
         <label className="flex flex-col min-w-[200px] text-white">
@@ -30,6 +55,8 @@ export default function InputBox() {
             type="number"
             placeholder="Enter your monthly investment amount"
             className="mt-2 p-3 rounded border-none bg-slate-700 text-white placeholder-gray-300"
+            onChange={handleMonthlyInvestmentChange}
+            value={monthlyInvestment}
           />
         </label>
         <label className="flex flex-col min-w-[200px] text-white">
